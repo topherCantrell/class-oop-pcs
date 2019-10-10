b950a2d9e15c2a658505917594069dbb3512dfd6
tophe <tophe@DESKTOP-LBAREV3>
2019-10-09 20:44:57 -0500

    tic tac toe example

---
+++ SevenNightClassPCS/oop-python/src/tictactoe5.py
@@ -0,0 +1,6 @@
+# How about the board? Just one board in existence? Different kinds of boards?
+# Network boards? GUI boards?
+
+# Move the board stuff into a class
+
+# Now main just wires up  ob jects. big bvang
